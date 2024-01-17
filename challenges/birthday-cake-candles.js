10066606c2960760b93750476be17633826bf7e2
Nate Sobeck <nsobeck910@gmail.com>
2024-01-17 09:59:21 -0500

    solve birthday-cakew-candles challenge

---
+++ challenges/birthday-cake-candles.js
@@ -0,0 +1,37 @@
+// You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+//* Example
+
+
+// The maximum height candles are  units high. There are  of them, so return .
+
+//* Function Description
+
+// Complete the function birthdayCakeCandles in the editor below.
+
+// birthdayCakeCandles has the following parameter(s):
+
+// int candles[n]: the candle heights
+// Returns
+
+// int: the number of candles that are tallest
+//* Input Format
+
+// The first line contains a single integer, , the size of .
+// The second line contains  space-separated integers, where each integer  describes the height of .
+
+function birthdayCakeCandles(candles) {
+  let tallest = 0
+  let result = 0
+
+  candles.forEach(candle => {
+    if (candle > tallest) {
+      tallest = candle
+      result = 1
+    } else if (candle === tallest) {
+      result += 1
+    }
+  })
+
+  return result
+}
