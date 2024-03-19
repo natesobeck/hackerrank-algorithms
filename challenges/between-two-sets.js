485d7597126d04ff3182b7dda278c52ab6ea084a
Nate Sobeck <nsobeck910@gmail.com>
2024-03-18 21:09:23 -0400

    solve between two sets

--- challenges/between-two-sets.js
+++ challenges/between-two-sets.js
@@ -1,16 +1,39 @@
 // https://www.hackerrank.com/challenges/between-two-sets/problem?isFullScreen=true
 
 function getTotalX(a, b) {
-  let result = []
-
-  for (let i = 0; i < a.length; i++) {
-    a.forEach(int => {
-      if (a[i] % int === 0) {
-        result.push(int)
-      }
-    })
-    console.log(result)
+  // find the range of ints I need checked
+  let rangeBottom, rangeTop, result = []
+  for(let i = 0; i < a.length; i++) {
+    if (!rangeBottom || a[i] > rangeBottom) rangeBottom = a[i]
   }
+  for(let i = 0; i < b.length; i++) {
+    if (!rangeTop || b[i] < rangeTop) rangeTop = b[i]
+  }
+  // now we need to iterate through all ints in our range
+  let tempArr = []
+  for(let intBeingConsidered = rangeBottom; intBeingConsidered <= rangeTop; intBeingConsidered++) {
+    // check if each element in a is a factor of intBeingConsidered and if it is store it in a temporary array
+    let factor = true
+    for(let j = 0; j < a.length; j++) {
+      if (intBeingConsidered % a[j] !== 0) {
+        factor = false
+      }
+    }
+    if (factor) tempArr.push(intBeingConsidered)
+  }
+
+  for(let i = 0; i < tempArr.length; i++) {
+    let divisible = true
+    for(let j = 0; j < b.length; j++) {
+      if (b[j] % tempArr[i] !== 0) {
+        divisible = false
+      }
+    }
+    if (divisible) result.push(tempArr[i])
+  }
+
+  console.log(result)
+  return tempArr.length
 }
 
-getTotalX([1, 2, 4], [60, 67, 89])
+getTotalX([2, 6], [24, 36])
