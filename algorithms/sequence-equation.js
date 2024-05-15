c3477d57b2c62e6e971054fa837d8725dc7cfb87
Nate Sobeck <nsobeck910@gmail.com>
2024-05-15 12:11:48 -0400

    solve sequence-equation

---
+++ algorithms/sequence-equation.js
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/permutation-equation/problem?isFullScreen=true
+
+/*
+
+1. let result = []
+
+2. Use a for loop to loop from x = 1 to the length of the provided array
+
+3. For each loop:
+  - const y = array.indexOf(x) + 1
+  - push y to the result array
+
+4. return result
+
+*/
+
+function permutationEquation(array) {
+  let result = []
+  let objArray = {}
+
+  for(let x = 1; x <= array.length; x++) {
+    objArray[array[x - 1]] = x
+  }
+
+  for(let x = 1; x <= array.length; x++) {
+    const y = objArray[objArray[x]]
+    result.push(y)
+  }
+
+  return result
+}
+
+console.log(permutationEquation([3, 1, 2]))
